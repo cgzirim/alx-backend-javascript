aa0b4dc99d27dc4f5767f49f2e331c097d7b35b1
Chigozirim <iamokwukwe@gmail.com>
2022-06-15 21:48:01 +0100

    Do advanced task

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -2,10 +2,10 @@ export default function iterateThroughObject(reportWithIterator) {
   const employeeList = [...Object.values(reportWithIterator)];
   const str = '';
 
-  for (let employee of employeeList) {
-    str.push("|");
+  for (const employee of employeeList) {
+    str.push(employee);
     if (employee !== employeeList[employeeList.length - 1]) {
-        str.push("|");
+      str.push('|');
     }
   }
   return str;
