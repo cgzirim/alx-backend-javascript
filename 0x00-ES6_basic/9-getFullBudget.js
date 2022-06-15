2a0c255201c1aab67292c71342b3e3188aa92485
Chigozirim <iamokwukwe@gmail.com>
2022-06-15 20:56:42 +0100

    Do task 9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,11 +4,11 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars() {
-      return `$${income}`;
+    getIncomeInDollars(int) {
+      return `$${int}`;
     },
-    getIncomeInEuros() {
-      return `${income} euros`;
+    getIncomeInEuros(int) {
+      return `${int} euros`;
     },
   };
 
