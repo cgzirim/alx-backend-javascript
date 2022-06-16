a8ab6bf3ee8dbf7d8c1071dd04f39177f0736e27
Chigozirim <iamokwukwe@gmail.com>
2022-06-16 14:24:52 +0100

    Do task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+    });
+}
