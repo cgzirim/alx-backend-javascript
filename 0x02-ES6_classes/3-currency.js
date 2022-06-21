8e1e4776d3a5ef250d9ae5bf09846ac9fdc2ee36
Chigozirim <iamokwukwe@gmail.com>
2022-06-21 22:18:23 +0100

    Do task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -9,20 +9,14 @@ export default class Currency {
   }
 
   set code(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
     this._code = value;
   }
 
   get name() {
-    return this._code;
+    return this._name;
   }
 
   set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
     this._name = value;
   }
 
