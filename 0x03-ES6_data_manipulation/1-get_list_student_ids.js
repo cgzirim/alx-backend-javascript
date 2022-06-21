15df71ce92e0162d4937a4a07c7c0b0cd13f0bfd
Chigozirim <iamokwukwe@gmail.com>
2022-06-21 12:10:06 +0100

    Do task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,12 @@
 export default function getListStudentIds(array) {
   /* Returns an array of ids from a list of object */
 
+  if (Array.isArray(array) === false
+  || typeof array !== 'object'
+  || array === null) {
+    return [];
+  }
+
   const ids = [];
   for (const element of array) {
     if (Object.keys(element).includes('id')) {
