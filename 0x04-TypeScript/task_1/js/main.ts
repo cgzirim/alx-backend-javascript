58bfc03a70d913668c0a1e3e7abb0d543b504fa3
Chigozirim <iamokwukwe@gmail.com>
2022-06-22 16:38:45 +0100

    Do task 5

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -39,3 +39,49 @@ class StudentClass {
         return this.firstName;
     }
 }
+
+// Task 5
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return 'Working from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Getting a coffee break';
+    }
+
+    workDirectorTasks(): string {
+        return 'Getting to director tasks';
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return 'Cannot work from home';
+    }
+    getCoffeeBreak(): string {
+        return 'Cannot have a break';
+    }
+    workTeacherTasks(): string {
+        return 'Getting to work';
+    }
+
+}
+
+function createEmployee(salary: number | string) {
+    if (typeof salary === 'string') salary = salary.substring(1)
+    if (salary <  500) return new Teacher;
+    return new Director;
+}
