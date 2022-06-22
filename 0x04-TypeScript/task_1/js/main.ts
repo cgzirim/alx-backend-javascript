b1d4ed9ac513aa855955db8f69c8ed5c71821bd2
Chigozirim <iamokwukwe@gmail.com>
2022-06-22 15:23:25 +0100

    Do task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,8 @@
+class Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: any;
+}
