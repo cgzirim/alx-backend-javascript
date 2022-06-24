208dd520e9cd2136814574feeabb02dfc9c41bff
Chigozirim <iamokwukwe@gmail.com>
2022-06-24 14:02:18 +0100

    Do task 5

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,17 @@
+export interface MajorCredits {
+    credits: number;
+    brand: 'Major';
+}
+
+export interface MinorCredits {
+    credits: number;
+    brand: 'Minor';
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): number {
+    return subject1.credits + subject2.credits;
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): number {
+    return subject1.credits + subject2.credits;
+}
