52eada0aa5661625173d1d382fd7600b2aa98897
Chigozirim <iamokwukwe@gmail.com>
2022-08-09 23:25:53 +0100

    Update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-process.stdout.write('Welcome to Holberton School, what is you name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
-  const string = process.stdin.read();
-  if (string !== null) {
-    process.stdout.write(`Your name is: ${string}`);
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
