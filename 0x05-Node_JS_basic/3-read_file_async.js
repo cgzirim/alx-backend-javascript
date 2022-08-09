fe9b772a8ff3e1670a51fbe48fcbaa441ade7deb
Chigozirim <iamokwukwe@gmail.com>
2022-08-09 14:43:55 +0100

    CountStudents function should return a promise

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,37 +2,47 @@ const fs = require('fs');
 const util = require('util');
 const readFile = util.promisify(fs.readFile);
 
-async function countStudents (path) {
-  let content;
-  try {
-    content = await readFile(path, 'utf-8');
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
+function countStudents (path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (error, data) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
 
-  const students = content.split(/\r?\n/).filter(Boolean).slice(1);
-  console.log(`Number of students: ${students.length}`);
+      const response = [];
+      const content = data.toString().split('\n');
 
-  const fields = {};
-  for (const student of students) {
-    const st_data = student.split(',');
+      const students = content.filter(Boolean).slice(1);
+      let message = `Number of students: ${students.length}`;
+      console.log(message);
 
-    const st_first_name = st_data[0];
-    const st_field = st_data[st_data.length - 1];
+      response.push(message);
 
-    if (fields[st_field]) {
-      fields[st_field][0] += 1;
-      fields[st_field].push(st_first_name);
-    } else {
-      fields[st_field] = [1, st_first_name];
-    }
-  }
-  for (const [k, v] of Object.entries(fields)) {
-    const first_names = v.slice(1).join(', ');
-    console.log(`Number of students in ${k}: ${v[0]}. List: ${first_names}`);
-  }
+      const fields = {};
+      for (const student of students) {
+        const st_data = student.split(',');
 
-  return readFile(path);
+        const st_first_name = st_data[0];
+        const st_field = st_data[st_data.length - 1];
+
+        if (fields[st_field]) {
+          fields[st_field][0] += 1;
+          fields[st_field].push(st_first_name);
+        } else {
+          fields[st_field] = [1, st_first_name];
+        }
+      }
+      for (const [k, v] of Object.entries(fields)) {
+        const first_names = v.slice(1).join(', ');
+        message = `Number of students in ${k}: ${v[0]}. List: ${first_names}`;
+        console.log(message);
+        response.push(message);
+      }
+
+      resolve(response);
+    });
+  });
 }
 
 module.exports = countStudents;
