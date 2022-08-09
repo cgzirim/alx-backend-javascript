0cf76e0f929b970475b3944998d8700d62ccc5a5
Chigozirim <iamokwukwe@gmail.com>
2022-08-09 10:34:52 +0100

    Update program

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is you name?\n');
 
+process.stdin.setEncoding('utf8');
+
 process.stdin.on('readable', () => {
   const string = process.stdin.read();
   if (string !== null) {
