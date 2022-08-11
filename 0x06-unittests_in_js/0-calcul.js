83d97e2625ae10e7307e81eb837de3cdae23f883
Chigozirim <iamokwukwe@gmail.com>
2022-08-11 16:16:35 +0100

    Initial commit

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,7 @@
+function calculateNumber(a, b) {
+    const first_num = Math.round(a);
+    const second_number = Math.round(b);
+    return first_num + second_number;
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/README.md
@@ -0,0 +1,1 @@
+$ Unittest is JS
