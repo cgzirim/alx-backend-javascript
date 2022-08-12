2721b4f81dd6125d2b2ac79614d6d5c10fe1f27b
Chigozirim <iamokwukwe@gmail.com>
2022-08-12 11:03:43 +0100

    Correct tests

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,25 +3,25 @@ const expect = chai.expect
 const calculateNumber = require('./1-calcul');
 
 describe("calculateNumber function:", function() {
-    describe("Calculates addition of two float:", function() {
+    describe("Tests SUM:", function() {
         it('Returns 6.', function() {
             expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
         });
     });
 
-    describe("calculates subtraction of two float:", function() {
+    describe("Tests SUBTRACT:", function() {
         it("Returns -4", function() {
             expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
         });
     });
 
-    describe("calculates division of two float:", function() {
+    describe("Tests DIVIDE:", function() {
         it("Returns 0.2", function() {
             expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
         });
     });
 
-    describe("calculates division by zero:", function() {
+    describe("Tests DIVIDE with zero:", function() {
         it("returns Error", function() {
             expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
         });
